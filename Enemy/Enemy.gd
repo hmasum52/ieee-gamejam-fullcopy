984969edfd1a086b52f66c440b7a35d8d2bf2e17
Sohaib03 <sohaibbinmusa@gmail.com>
2023-01-07 01:18:26 +0600

    Added Enemy Death Animation, Health

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,6 +1,13 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 var knockback: Vector2 = Vector2.ZERO
+
+onready var stats = $Stats
+
+func _ready():
+	print(stats.max_health)
+	
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200*delta)
@@ -8,4 +15,12 @@ func _physics_process(delta):
 
 
 func _on_HurtBox_area_entered(area):
+	stats.health -= area.damage
 	knockback = area.knockback_vector *120
+
+
+func _on_Stats_no_health():
+	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	enemyDeathEffect.global_position = global_position
+	get_parent().add_child(enemyDeathEffect)
