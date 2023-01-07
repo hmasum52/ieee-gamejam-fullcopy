f484f8186215b30ef7360f0ef71026bd67250346
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2023-01-08 02:35:32 +0600

    rearranged project

--- World/World1.gd
+++ World/World1.gd
@@ -6,7 +6,6 @@ onready var canvasmodulate = $CanvasModulate
 func _ready():
 	print("World is ready")
 	$CanvasModulate.visible = false
-	print($CanvasModulate.visible)
 
 
 func _on_Map2_area_entered(area):
@@ -15,7 +14,4 @@ func _on_Map2_area_entered(area):
 
 func _on_Map2_area_exited(area):
 	$CanvasModulate.visible = false
-
-
-func _on_Map1_area_entered(area):
-	$CanvasModulate.visible = false
+	print("Map2 exited")
