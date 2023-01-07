d0859715cfac54fcd259df76cc3daf723e197869
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2023-01-08 02:55:08 +0600

    added player light in map2

--- World/World1.gd
+++ World/World1.gd
@@ -6,12 +6,15 @@ onready var canvasmodulate = $CanvasModulate
 func _ready():
 	print("World is ready")
 	$CanvasModulate.visible = false
+	$Maps/Player/Light2D.visible = false
 
 
 func _on_Map2_area_entered(area):
 	$CanvasModulate.visible  = true
+	$Maps/Player/Light2D.visible = true
 	
 
 func _on_Map2_area_exited(area):
 	$CanvasModulate.visible = false
+	$Maps/Player/Light2D.visible = false
 	print("Map2 exited")
