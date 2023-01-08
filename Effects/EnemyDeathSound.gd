56ba2789f11f000b5cae0a36ac2e080999d112b9
TamimEhsan <tamimehsan99@gmail.com>
2023-01-08 11:49:12 +0600

    added enemy death sound

--- Effects/EnemyDeathSound.gd
+++ Effects/EnemyDeathSound.gd
@@ -8,7 +8,7 @@ export(String) var start = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	play()
+	play(0.15)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
